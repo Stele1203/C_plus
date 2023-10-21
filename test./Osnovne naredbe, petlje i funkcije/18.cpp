9c60cf47aaa812d93fa2c3cf09997edb79ed945d
Stefan <52536505+Stele1203@users.noreply.github.com>
2023-10-21 19:42:26 +0200

    Create 18.cpp
    Napisati funkciju bool trougao(float x1, float y1, float x2, float y2, float x3, float y3) koja
    provjerava da li postoji trougao čija su tjemena (x1, y1), (x2, y2) i (x3, y3).

---
+++ test./Osnovne naredbe, petlje i funkcije/18.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool trougao(float x1, float y1, float x2, float y2, float x3, float y3) {
+
+    float povrsina = 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+
+    return povrsina > 0.0;
+}
+
+int main() {
+    float x1, y1, x2, y2, x3, y3;
+
+    cout << "Unesite koordinate tjemena trougla (x1, y1): ";
+    cin >> x1 >> y1;
+    cout << "Unesite koordinate tjemena trougla (x2, y2): ";
+    cin >> x2 >> y2;
+    cout << "Unesite koordinate tjemena trougla (x3, y3): ";
+    cin >> x3 >> y3;
+
+    bool postojiTrougao = trougao(x1, y1, x2, y2, x3, y3);
+
+    if (postojiTrougao) {
+        cout << "Postoji trougao sa zadanim tjemena." << endl;
+    } else {
+        cout << "Ne postoji trougao sa zadanim tjemena." << endl;
+    }
+
+    return 0;
+}
