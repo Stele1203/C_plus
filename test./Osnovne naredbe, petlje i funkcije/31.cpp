e2343d502a3393c7f3df09e95a6e2430d9cd6d7e
Stefan <52536505+Stele1203@users.noreply.github.com>
2023-10-21 22:01:53 +0200

    Update 31.cpp
    duplikati

--- test./Osnovne naredbe, petlje i funkcije/31.cpp
+++ test./Osnovne naredbe, petlje i funkcije/31.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+bool duplikati(int* a, int n){
+    bool duplikati= false;
+
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]==a[j]){
+                duplikati=true;
+                return duplikati;
+            }
+        }
+    }
+    return duplikati;
+
+}
+
+
+
+
+int main() {
+
+  int etc[]={5,4,4,2,3};
+
+
+
+  if(duplikati(etc,5)){
+      cout<<"Ima duplikata";
+  }else{
+      cout<<"Nema duplikata";
+  }
+
+    return 0;
+}
