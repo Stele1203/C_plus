ed09bf841707826b840c0ae927c26bb34b68857d
Stefan <52536505+Stele1203@users.noreply.github.com>
2024-04-08 14:19:47 +0200

    Update zad5.cpp

--- Binarna Stabla/zad5.cpp
+++ Binarna Stabla/zad5.cpp
@@ -1,13 +1,33 @@
-struct cvor* najmanjaDubina(struct cvor* korijen, int trenDub = 0) {
-    if (korijen == NULL)
-        return NULL;
+vector<pair<cvor*, int>> dfsNajmanjaDubina(struct cvor* korijen,int dubina){
+    vector<pair<cvor*,int>> niz;
+    if(korijen== NULL) {
+        return niz;
+    }
+    if(korijen->lijevi == NULL && korijen->desni == NULL){
+        niz.push_back(make_pair(korijen, dubina));
+        return niz;
+    }
 
-    if (korijen->lijevi == NULL && korijen->desni == NULL)
-        return korijen;
+    vector<pair<cvor*,int>> lijevi= dfsNajmanjaDubina(korijen->lijevi,dubina+1);
+    vector<pair<cvor*,int>> desni= dfsNajmanjaDubina(korijen->desni,dubina+1);
 
-    struct cvor* levo = najmanjaDubina(korijen->lijevi, trenDub + 1);
-    if (levo != NULL)
-        return levo;
+    niz.insert(niz.end(),lijevi.begin(),lijevi.end());
+    niz.insert(niz.end(),desni.begin(),desni.end());
 
-    return najmanjaDubina(korijen->desni, trenDub + 1);
+    return niz;
+
+}
+
+bool komparator(const pair<cvor*, int>& a, const pair<cvor*, int>& b) {
+    if(a.second != b.second)
+        return a.second<b.second;
+    else
+        return a.first->vrijednost>b.first->vrijednost;
+}
+
+struct cvor* listNajmanjaDubina(struct cvor* korijen){
+    vector<pair<cvor*,int>> niz=dfsNajmanjaDubina(korijen,0);
+    sort(niz.begin(),niz.end(), komparator);
+
+    return niz[0].first;
 }
